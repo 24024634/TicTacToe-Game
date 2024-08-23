0fb830a1ffac3a81c8bb4451cd9812771ffe5e0e
24024634 <162483823+24024634@users.noreply.github.com>
2024-08-23 18:59:55 +0200

    Add files via upload

---
+++ src/TicTac.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class TicTac {
+
+    public static void main(String[] args) {
+        char[][] gameBoard = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+        };
+
+        char currentPlayer = 'X';
+        boolean gameISUnderway = true;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (gameISUnderway) {
+            System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " +
+                    gameBoard[0][2]);
+
+            System.out.println("--+---+--");
+
+
+            System.out.println(gameBoard[1][0] + " | " +
+                    gameBoard[1][1] + " | " + gameBoard[1][2]);
+
+            System.out.println("--+---+--");
+
+
+            System.out.println(gameBoard[2][0] + " | " +
+                    gameBoard[2][1] + " | " + gameBoard[2][2]);
+
+            int shift;
+            while (true) {
+                System.out.println("Player " + currentPlayer + ", enter your first move (Use a number from 1 to 9): ");
+                shift = scanner.nextInt();
+
+                if (shift < 1 || shift > 9) {
+                    System.out.println("Invalid move, enter a number from 1 to 9");
+                    continue;
+                }
+
+                int row = (shift - 1) / 3;
+                int col = (shift - 1) % 3;
+
+                if (gameBoard[row][col] != 'X' && gameBoard[row][col] != 'O') {
+                    gameBoard[row][col] = currentPlayer;
+                    break;
+                } else {
+                    System.out.println("Invalid move, The position is already taken, Try again");
+                }
+            }
+
+            boolean hasWon = false;
+            for (int i = 0; i < 3; i++) {
+                if ((gameBoard[i][0] == currentPlayer && gameBoard[i][1] == currentPlayer && gameBoard[i][2] == currentPlayer) ||
+                        (gameBoard[0][i] == currentPlayer && gameBoard[1][i] == currentPlayer && gameBoard[2][i] == currentPlayer)) {
+                    hasWon = true;
+                    break;
+                }
+            }
+
+            if ((gameBoard[0][0] == currentPlayer && gameBoard[1][1] == currentPlayer && gameBoard[2][2] == currentPlayer) ||
+                    (gameBoard[0][2] == currentPlayer && gameBoard[1][1] == currentPlayer && gameBoard[2][0] == currentPlayer)) {
+                hasWon = true;
+            }
+
+            if (hasWon) {
+                System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
+                System.out.println("--+---+--");
+
+                System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
+                System.out.println("--+---+--");
+
+
+                System.out.println(gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
+                System.out.println("Congratulations " + currentPlayer + " You have won!");
+                gameISUnderway = false;
+            } else {
+                boolean itisaDraw = true;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (gameBoard[i][j] != 'X' && gameBoard[i][j] != 'O') {
+                            itisaDraw = false;
+                        }
+                    }
+                }
+                if (itisaDraw) {
+                    System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
+
+                    System.out.println("--+---+--");
+
+                    System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
+
+                    System.out.println("--+---+--");
+
+
+                    System.out.println(gameBoard[2][0] + " | " +
+                            gameBoard[2][1] + " | " + gameBoard[2][2]);
+
+                    System.out.println("It is a draw!");
+                    gameISUnderway = false;
+                }
+            }
+
+            if (currentPlayer == 'X') {
+                currentPlayer = 'O';
+            } else {
+                currentPlayer = 'X';
+            }
+        }
+    }
+}
